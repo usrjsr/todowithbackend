49cf1ee43c87d10c81d064c7a9cf1e2cd7af9055
usrjsr <utkarshsinghdau@gmail.com>
2025-08-07 22:56:36 +0530

    Updated backend with fixed MongoDB URI and route handlers

--- data/database.js
+++ data/database.js
@@ -4,8 +4,8 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       dbName: "todoproject",
-    });
-    console.log("✅ Database is connected");
+    }).then((c)=> console.log(`✅ Database is connected with ${c.connection.host}`));
+    
   } catch (err) {
     console.error("❌ Database connection failed:", err.message);
     process.exit(1);
